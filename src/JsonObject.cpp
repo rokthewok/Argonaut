49066e5fd7e679bc18aee348002afad51a3762a6
rokthewok <jqruffer@vt.edu>
2013-05-31 19:58:24 -0400

    added JsonObject class for parser. Shall be main interface for user obtaining Json data.

---
+++ src/JsonObject.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  JsonObject.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/30/2013 12:50:21 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "JsonObject.h"
